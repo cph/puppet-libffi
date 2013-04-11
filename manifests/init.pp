7ee106ef43031feb5485190a92668a4d02e3e12d
Kendall Park <kendall.park@cph.org>
2013-04-11 16:13:49 -0500

    First stab at libffi puppet module.

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,4 +1,37 @@
-# This is a placeholder class.
-class template {
-  anchor { 'Hello_World': }
+# libffi 3.0.12
+# There are issues with 3.0.13, so we're rolling back to the last version
+#
+#  include libffi
+class libffi {
+  include boxen::config
+
+  $libffi_path = "${boxen::config::home}/libffi"
+  $libffi_version = 'v3.0.12'
+  #$libffi_patch = "patch -Np1 -i 3.0.13.patch"
+
+  repository { 'libffi repo':
+    source   => 'atgreen/libffi',
+    provider => 'git',
+    path     => $libffi_path,
+  }
+
+  exec { 'checkout libffi':
+    command => "git fetch origin && git reset --hard && git checkout -b '${libffi_version}' '${libffi_version}'",
+    cwd     => $libffi_path,
+    before  => Exec['install libffi'],
+    unless  => "git branch | grep -c '* ${libffi_version}'",
+  }
+
+  exec { 'install libffi':
+    command  => "${libffi_path}/configure --prefix=${libffi_path} --disable-static --enable-debug && make && make check && make install && echo ${libffi_version} > ${libffi_path}/.version",
+    unless   => "grep -c ${libffi_version} ${libffi_path}/.version",
+    before   => Exec['add installed libffi files to git repo'],
+  }
+
+  exec {'add installed libffi files to git repo':
+    command => 'git add .',
+    cwd     => $libffi_path,
+    before  => Exec['install glib']
+  }
+
 }
--- spec/classes/template_spec.rb
+++ spec/classes/template_spec.rb
@@ -2,8 +2,12 @@ require 'spec_helper'
 # Rename this file to classname_spec.rb
 # Check other boxen modules for examples
 # or read http://rspec-puppet.com/tutorial/
-describe 'template' do
+describe 'libffi' do
   it do
-    should contain_anchor('Hello_World')
+    #should include_class("boxen::config")
+    #should contain_exec("libffi repo")
+    #should contain_exec("checkout libffi")
+    #should contain_exec("install libffi")
+    #should contain_exec("add installed libffi files to git repo")
   end
 end
